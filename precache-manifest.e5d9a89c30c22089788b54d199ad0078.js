2ce5ed3631ef88b7acdd717268f79f68e48ee9da
Poojanavgurukul <Pooja17@navgurukul.org>
2018-10-12 23:09:11 +0530

    Updates

---
+++ precache-manifest.e5d9a89c30c22089788b54d199ad0078.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13a0cf08fb7dc01689eff486071e63e8",
+    "url": "/emoji/static/media/logo.13a0cf08.jpg"
+  },
+  {
+    "revision": "4164628e033be50d958366fdb012768b",
+    "url": "/emoji/static/media/back.4164628e.jpg"
+  },
+  {
+    "revision": "e1be33dba687f3227822",
+    "url": "/emoji/static/js/runtime~main.e1be33db.js"
+  },
+  {
+    "revision": "ecc1ec5d4a81ad6250c4",
+    "url": "/emoji/static/js/main.ecc1ec5d.chunk.js"
+  },
+  {
+    "revision": "88e405b5d6b56a532528",
+    "url": "/emoji/static/js/1.88e405b5.chunk.js"
+  },
+  {
+    "revision": "ecc1ec5d4a81ad6250c4",
+    "url": "/emoji/static/css/main.b2f293e6.chunk.css"
+  },
+  {
+    "revision": "88e405b5d6b56a532528",
+    "url": "/emoji/static/css/1.069ded93.chunk.css"
+  },
+  {
+    "revision": "1131c2db6811368cc414787f44b074d9",
+    "url": "/emoji/index.html"
+  }
+];
